d43f4a38bbfd1e766b2ad2124b9a295c84cd9861
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-01-30 20:20:11 +0100

    add enum attributes to ceremony db

--- app/models/ceremony.rb
+++ app/models/ceremony.rb
@@ -1,5 +1,9 @@
 class Ceremony < ApplicationRecord
-  belongs_to :user
+  enum burial_type: { ground_burial: 0, cremation: 1, space_burial: 2, tree_planting: 3,
+                      cryonics: 4, memorial_diamond: 5, vinyl_press: 6, body_donation: 7, sky_burial: 8 }
 
+  enum ceremony_type: { funeral: 0, memorial: 1 }
+
+  belongs_to :user
   has_many :contacts, through: :ceremony
 end
