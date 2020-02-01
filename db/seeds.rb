3d5c66f5389ac168a021f722425937b26135a57e
Sergejs Dementjevs <dozornik88@gmail.com>
2020-02-01 11:49:06 +0100

    added a popup window for the valuables

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 wolly = User.create!(legal_name:'wally', email: 'wally12@notwally.com', password:'wally123', birthdate: Date.new(2007,5,12))
-watches = Valuable.create!(title:'watch', image_url: 'wallynotwally.com', value:'12', description:"Rolex")
+watches = Valuable.create!(user_id: wolly.id, title:'watch', image_url: 'wallynotwally.com', value:'12', description:"Rolex")
