963c1355ac9ea01a693db6ff59cdbca6effee25b
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-02-05 19:02:26 +0100

    front end atetmpts on ceremony form

--- app/models/ceremony.rb
+++ app/models/ceremony.rb
@@ -1,9 +1,9 @@
 class Ceremony < ApplicationRecord
-  enum burial_type: { ground_burial: 0, cremation: 1, space_burial: 2, tree_planting: 3,
-                      cryonics: 4, memorial_diamond: 5, vinyl_press: 6, body_donation: 7, sky_burial: 8 }
+  enum burial_type: { burial: 0, cremation: 1, space_burial: 2, tree_planting: 3,
+                      cryonics: 4, memorial_diamond: 5, vinyl_press: 6, donation: 7, sky_burial: 8 }
 
   enum ceremony_type: { funeral: 0, memorial: 1, life_celebration: 2, wake: 3,
-                        grave_side: 4, scattering: 5, viewing: 6 }
+                        grave_side: 4, scattering: 5 }
 
   belongs_to :user
   belongs_to :speaker, class_name: 'Contact', foreign_key: 'speaker_id'
