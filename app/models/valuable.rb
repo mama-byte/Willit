3f106f94ea783568c42afc20f76aabb1980df851
Sergejs Dementjevs <dozornik88@gmail.com>
2020-02-05 13:37:11 +0100

    fully functioning valuables with categories

--- app/models/valuable.rb
+++ app/models/valuable.rb
@@ -1,7 +1,12 @@
   class Valuable < ApplicationRecord
-  enum category: { electronics: 0, household: 1, jewelry: 2, vehicles: 3}
+  enum category: { electronics: 0, housewares: 1, jewelry: 2, vehicles: 3}
 
-  enum item: { computer: 0, tv: 1, bikes: 2}
+  enum item: { instruments: 0, books: 1, coins: 2, vinyls: 3, games: 4, miles: 5,
+    computer: 6, phone: 7, camera: 8, television: 9, game_console: 10,
+    stereo: 11, furniture: 12, appliance: 13, artwork: 14, antiques: 15, clothing: 16,
+    kitchenwares: 17, rings: 18, braclets: 19, earings: 20, necklaces: 21,
+    watches: 22, cufflinks: 23, cars: 24, bikes: 25, motorbikes: 26, snowboards: 27,
+    scooters: 28, boats: 29}
 
   belongs_to :user
 
@@ -9,3 +14,4 @@
   has_many :contacts, through: :contact_valuables
   mount_uploader :photo, PhotoUploader
 end
+#computer: 0, tv: 1, bikes: 2, artwork: 3
