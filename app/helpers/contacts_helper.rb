124a9e20f0d249669c20becb51220421679bd2e1
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-02-12 19:21:03 +0100

    add blank method

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -17,10 +17,10 @@ def contact_progress
 
 def profile_progress
   complete = []
-     complete << current_user.legal_name if !current_user.legal_name.empty?
-     complete << current_user.nickname if !current_user.nickname.empty?
-     complete << current_user.nationality if !current_user.nationality.empty?
-     complete << current_user.pronouns if !current_user.pronouns.empty?
+     complete << current_user.legal_name if !current_user.legal_name.blank?
+     complete << current_user.nickname if !current_user.nickname.blank?
+     complete << current_user.nationality if !current_user.nationality.blank?
+     complete << current_user.pronouns if !current_user.pronouns.blank?
      if complete.length == 4
       progress = 100
      elsif complete.length == 3
@@ -41,11 +41,11 @@ def ceremony_progress
     progress = 0
   else
     complete = []
-    complete << ceremony.ceremony_type if !ceremony.ceremony_type.empty?
-    complete << ceremony.burial_type if !ceremony.burial_type.empty?
-    complete << ceremony.details if !ceremony.details.empty?
-    complete << ceremony.epitaph if !ceremony.epitaph.empty?
-    complete << ceremony.press_release if !ceremony.press_release.empty?
+    complete << ceremony.ceremony_type if !ceremony.ceremony_type.blank?
+    complete << ceremony.burial_type if !ceremony.burial_type.blank?
+    complete << ceremony.details if !ceremony.details.blank?
+    complete << ceremony.epitaph if !ceremony.epitaph.blank?
+    complete << ceremony.press_release if !ceremony.press_release.blank?
     if complete.length == 5
       progress = 100
     elsif complete.length == 3 || complete.length == 4
