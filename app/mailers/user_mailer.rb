84b9c889a94452cd33208f639d7aab44678a450e
Sergejs Dementjevs <dozornik88@gmail.com>
2020-02-13 20:15:18 +0100

    done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'contact@whenotwillit.com'
+  default from: 'contact@whynotwillit.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
