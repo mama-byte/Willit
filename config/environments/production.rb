2aa48d8db4af61b580fa73c9b28e9601b90a188a
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-02-15 14:32:44 +0100

    prepare for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   Rails.application.configure do
     # ...
     config.action_mailer.delivery_method     = :postmark
-    config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
+    config.action_mailer.postmark_settings   = { api_token: ENV["POSTMARK_API_TOKEN"] }
     config.action_mailer.default_url_options = { host: "willit.herokuapp.com" }
     # or your custom domain name eg. "www.yourdomain.com"
   end
