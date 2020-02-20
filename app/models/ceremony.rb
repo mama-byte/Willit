4ee0b4ae4ac6d8d88cf3023e213dcebe960935fe
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-02-20 22:11:24 +0100

    changed the database structure for ceremony so that strings are displayed in the show

--- app/models/ceremony.rb
+++ app/models/ceremony.rb
@@ -1,9 +1,9 @@
 class Ceremony < ApplicationRecord
-  enum burial_type: { burial: 0, cremation: 1, space_burial: 2, green: 3,
-                      cryonics: 4, diamond: 5, vinyl_press: 6, donation: 7 }
+  enum burial_type: { "Burial" => 0, "Cremation" => 1, "Space Burial" => 2, "Green" => 3,
+                      "Cryonics" => 4, "Diamond" => 5, "Vinyl Press" => 6, "Donation" => 7 }
 
-  enum ceremony_type: { funeral: 0, memorial: 1, celebration: 2, wake: 3,
-                        grave_side: 4, scattering: 5 }
+  enum ceremony_type: { "Funeral" => 0, "Memorial" => 1, "Celebration" => 2, "Wake" => 3,
+                        "Graveside" => 4, "Scattering" => 5 }
 
   belongs_to :user
   belongs_to :speaker, class_name: 'Contact', foreign_key: 'speaker_id'
