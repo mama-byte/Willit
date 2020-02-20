6eaa63375373bbd41a7c481a56b30f49a55f4f7d
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-02-20 01:55:06 +0100

    fixed ceremony form

--- app/models/ceremony.rb
+++ app/models/ceremony.rb
@@ -1,6 +1,6 @@
 class Ceremony < ApplicationRecord
   enum burial_type: { burial: 0, cremation: 1, space_burial: 2, green: 3,
-                      cryonics: 4, diamond: 5, vinyl_press: 6, donation: 7, sky_burial: 8 }
+                      cryonics: 4, diamond: 5, vinyl_press: 6, donation: 7 }
 
   enum ceremony_type: { funeral: 0, memorial: 1, celebration: 2, wake: 3,
                         grave_side: 4, scattering: 5 }
